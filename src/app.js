d6c3f791a5c9190e32dc8299fde60ae2726d0c6f
Leandro Rocha <prog.leandro@gmail.com>
2020-07-23 22:54:12 -0300

    Route props

--- src/app.js
+++ src/app.js
@@ -1,91 +1,133 @@
-'use strict'
+"use strict";
 
-import React, { PureComponent } from 'react'
-import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom'
+import React, { PureComponent } from "react";
+import { BrowserRouter, NavLink, Route, Switch } from "react-router-dom";
 
-import './css/style.css'
+import "./css/style.css";
 
 class App extends PureComponent {
-  constructor () {
-    super()
+  constructor() {
+    super();
     this.state = {
-      title: '...',
-      Component: 'div'
-    }
+      title: "...",
+      Component: "div",
+    };
   }
 
-  getTitle () {
+  getTitle() {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve('My app with async / await!')
-      }, 2000)
-    })
+        resolve("My app with async / await!");
+      }, 2000);
+    });
   }
 
-  async componentDidMount () {
-    const title = await import('components/title')
+  async componentDidMount() {
+    const title = await import("components/title");
 
     this.setState({
       title: await this.getTitle(),
-      Component: title.default
-    })
+      Component: title.default,
+    });
   }
 
-  render () {
+  render() {
     return (
       <BrowserRouter>
         <div>
           {/* <this.state.Component>{this.state.title}</this.state.Component> */}
           <ul>
-            <li> <Link  to='/' exact>Home</Link></li>
-            <li> <Link to='/sobre'>Sobre</Link></li>
-            <li> <Link to='/blog'>Blog</Link></li>
-            <li> <Link to='/contato'>Contato</Link></li>
+            <li>
+              {" "}
+              <Link to="/" exact>
+                Home
+              </Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/sobre">Sobre</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/blog">Blog</Link>
+            </li>
+            <li>
+              {" "}
+              <Link to="/contato">Contato</Link>
+            </li>
           </ul>
 
-        <Switch>
-          <Route path="/" component={Home} exact/>
-          <Route path="/(sobre|contato)" component={Page} />
-          <Route path="/blog" component={Blog} />
-          <Route component={Erro404} />
-        </Switch>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/(sobre|contato)" component={Page} />
+            <Route path="/blog" component={Blog} />
+            <Route component={Erro404} />
+          </Switch>
         </div>
       </BrowserRouter>
-    )
+    );
   }
 }
 
-const Home = () => (<h1>Home</h1>)
-const Sobre = () => (<h1>Sobre</h1>)
-const Page = ({ match }) => (<h1>{match.url}</h1>)
-const Contato = () => (<h1>Contato</h1>)
-const Blog = () => (
-    <div>
-      <h1>Blog</h1>
-      <ul>
-        <li><Link to="/blog/post-1">Post 1</Link></li>
-        <li><Link to="/blog/post-2">Post 2</Link></li>
-      </ul>
-      <Switch>
-        <Route exact path="/blog" render={ () => <NoPosts numberOfPost={2} />}/>
-        <Route path="/blog/:post(post-1|post-2)" component={Post} />
-        <Route component={Post404} />
-      </Switch>
-    </div>
-  )
+const Home = ({ match }) => (
+  <div>
+    {console.log("Home match", match)}
+    <h1>Home</h1>
+  </div>
+);
 
- const Post = (props) => (
-   <div>
-     {console.log(props)}
-     <h2>{props.match.params.post}</h2>
-   </div>
- )
+const Page = ({ match }) => (
+  <div>
+    {console.log("Page match", match)}
+    <p>{match.url}</p>
+  </div>
+);
 
-const NoPosts = ({numberOfPost}) => (
-<p>Selecione um  dos {numberOfPost} post</p>
-)
-const Erro404 = () => (<h1>Página não encontrada</h1>)
-const Post404 = () => (<h1>Post não encontrado</h1>)
+const Blog = ({ match }) => (
+  <div>
+    {console.log("Home match", match)}
+    <h1>Blog</h1>
+    <ul>
+      <li>
+        <Link to="/blog/post-1">Post 1</Link>
+      </li>
+      <li>
+        <Link to="/blog/post-2">Post 2</Link>
+      </li>
+    </ul>
+    <Switch>
+      <Route exact path="/blog" render={() => <NoPosts numberOfPost={2} />} />
+      <Route path="/blog/:post(post-1|post-2)" component={Post} />
+      <Route component={Post404} />
+    </Switch>
+  </div>
+);
+
+const Post = ({ match }) => (
+  <div>
+    {console.log("Post match", match)}
+    <h2>{match.params.post}</h2>
+  </div>
+);
+
+const NoPosts = ({ numberOfPost }) => (
+  <div>
+
+    <p>Selecione um dos {numberOfPost} post</p>
+  </div>
+);
+const Erro404 = ({ match }) => (
+  <div>
+    {console.log("Home match", match)}
+    <h1>Página não encontrada</h1>
+  </div>
+);
+const Post404 = ({ match }) => (
+  <div>
+    {console.log("Post404 match", match)}
+    <h1>Post não encontrado</h1>
+  </div>
+);
 
 // const Link = (props) => (
 //   <NavLink activeClassName="active-link"   {...props} />
@@ -93,10 +135,19 @@ const Post404 = () => (<h1>Post não encontrado</h1>)
 
 const Link = (props) => (
   <Route path={props.to} exact={props.exact}>
-  {({match, history}) => (
-    <a href={props.to} style={match ? { color: 'red'}: null} onClick={(e) => {e.preventDefault(); history.push(props.to)} }>{props.children}</a>
-  )}
-</Route>
-)
+    {({ match, history }) => (
+      <a
+        href={props.to}
+        style={match ? { color: "red" } : null}
+        onClick={(e) => {
+          e.preventDefault();
+          history.push(props.to);
+        }}
+      >
+        {props.children}
+      </a>
+    )}
+  </Route>
+);
 
-export default App
+export default App;
